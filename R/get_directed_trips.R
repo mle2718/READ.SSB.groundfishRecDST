a05e3077b3d8db36daa42fb1e7c94b24ff21e0d8
kimberly-bastille <kimberly.bastille@noaa.gov>
2024-05-29 10:36:53 -0400

    add proj

---
+++ R/get_directed_trips.R
@@ -0,0 +1,20 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+get_directed_trips <- function() {
+  print("Hello, directed trips code!")
+
+  print("Can use Rstata package to run stata code in R more details at https://ignacioriveros1.github.io/r/2020/03/22/r_and_stata.html")
+}
